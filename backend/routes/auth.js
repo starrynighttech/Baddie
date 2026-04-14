d7312c2edde0fcb69bb40f341953966278d1deeb
starrynighttech <starrynight8531@gmail.com>
2026-04-15 01:49:56 +0800

    Create auth.js

---
+++ backend/routes/auth.js
@@ -0,0 +1,29 @@
+const router = require("express").Router()
+const User = require("../models/User")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+router.post("/register", async (req, res) => {
+  const { email, password } = req.body
+
+  const hashed = await bcrypt.hash(password, 10)
+  const user = await User.create({ email, password: hashed })
+
+  res.json(user)
+})
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+  if (!user) return res.status(400).json({ msg: "No user" })
+
+  const valid = await bcrypt.compare(password, user.password)
+  if (!valid) return res.status(400).json({ msg: "Wrong password" })
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+
+  res.json({ token })
+})
+
+module.exports = router
