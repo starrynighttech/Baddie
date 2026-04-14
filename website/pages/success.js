da5634fd8c74154334f59d3bfc9dd20fea2bf6a7
starrynighttech <starrynight8531@gmail.com>
2026-04-15 01:44:35 +0800

    Create success.js

---
+++ website/pages/success.js
@@ -0,0 +1,14 @@
+export default function Success() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "white",
+      background: "black"
+    }}>
+      <h1>Payment Successful 🎉</h1>
+    </div>
+  )
+}
